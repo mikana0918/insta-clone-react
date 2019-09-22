02e1d4fce3976a9a484f2796cf26c26fef462c0e
mikana0918 <>
2019-09-22 14:03:42 +0900

    photo feature

--- CameraExample.js
+++ CameraExample.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image,Dimensions, Linking, StatusBar, ScrollView,TouchableWithoutFeedback,TouchableOpacity,ImageBackground,Modal,TouchableHighlight } from 'react-native';
+import { CameraRoll, StyleSheet, Text, View, Image,Dimensions, Linking, StatusBar, ScrollView,TouchableWithoutFeedback,TouchableOpacity,ImageBackground,Modal,TouchableHighlight } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
+// import Permissions from 'react-native-permissions';
 import { Header,Icon,SearchBar,Input,Button } from 'react-native-elements';
 
 export default class CameraExample extends React.Component {
@@ -15,15 +16,33 @@ export default class CameraExample extends React.Component {
     type: Camera.Constants.Type.back,
   };
 
-  async componentDidMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
-  }
+//   async componentDidMount() {
+//     const { status } = await Permissions.askAsync(Permissions.CAMERA);
+//     this.setState({ hasCameraPermission: status === 'granted' });
+//   }
+
+    
+    async takePicture() {       
+        console.log('Button Pressed');
+        if (this.camera) {
+            const { status } = await Permissions.askAsync(Permissions.CAMERA);
+            this.setState({ hasCameraPermission: status === 'granted' });
+
+            console.log('Taking photo');
+            const options = { quality: 1, base64: true, fixOrientation: true, exif: true};
+            await this.camera.takePictureAsync(options).then((photo)=>{
+                console.log(photo.uri);
+                CameraRoll.saveToCameraRoll(photo.uri, 'photo')
+                console.log('save to cameraroll?');
+            });
+           
+            }
+        }
+        
 
   render() {
     const { hasCameraPermission } = this.state;
     const {navigate} = this.props.navigation;
-    console.warn({navigate});
     if (hasCameraPermission === null) {
       return <View />;
     } else if (hasCameraPermission === false) {
@@ -44,7 +63,12 @@ export default class CameraExample extends React.Component {
                                 />      
                 </TouchableOpacity>
             </View>
-          <Camera style={{ height:'80%',  bottom:0 }} type={this.state.type}>
+
+          <Camera 
+          style={{ height:'80%',  bottom:0 }} type={this.state.type}
+          ref={ref => {
+            this.camera = ref;
+          }}>
             <View
               style={{
                 flex: 1,
@@ -65,12 +89,19 @@ export default class CameraExample extends React.Component {
                         : Camera.Constants.Type.back,
                   });
                 }}>
+
+  
                 <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
               </TouchableOpacity>
             </View>
           </Camera>
+
           <View style={{height:'18%', padding:10, backgroundColor:'pink', flexDirection:'row', justifyContent: 'space-between', padding: '2%', }}>
-                <TouchableOpacity style={{backgroundColor:'gray', height:'100%'}}>
+                <TouchableOpacity 
+                style={{backgroundColor:'gray', height:'100%'}}
+                onPress={() => {
+                    this.takePicture();
+                  }}>
                     <ImageBackground  style={{alignSelf:'center', width:32, height: 32, borderRadius: 32 / 2, borderColor:'white',borderWidth:3,padding:3, shadowColor: 'gray', shadowOffset: { width: 7, height: 9 }, shadowOpacity: 0.4, shadowRadius: 5, elevation: 2 }}>
                             <Icon
                                 name='camera'
@@ -81,7 +112,10 @@ export default class CameraExample extends React.Component {
                     </ImageBackground>
                 </TouchableOpacity>
 
-              <TouchableOpacity>
+              <TouchableOpacity
+               onPress={() => {
+                this.takePicture();
+              }}>
                     <ImageBackground  style={{alignSelf:'center', width:50, height: 50, borderRadius: 50 / 2, borderColor:'white',borderWidth:3,padding:3, shadowColor: 'gray', shadowOffset: { width: 7, height: 9 }, shadowOpacity: 0.4, shadowRadius: 5, elevation: 2 }}>
                             <Icon
                                 name='camera'
@@ -92,7 +126,10 @@ export default class CameraExample extends React.Component {
                     </ImageBackground>
                 </TouchableOpacity>
 
-                <TouchableOpacity>
+                <TouchableOpacity
+                 onPress={() => {
+                    this.takePicture();
+                  }}>
                     <ImageBackground  style={{alignSelf:'right', width:32, height: 32, borderRadius: 32 / 2, borderColor:'white',borderWidth:3,padding:3, shadowColor: 'gray', shadowOffset: { width: 7, height: 9 }, shadowOpacity: 0.4, shadowRadius: 5, elevation: 2 }}>
                             <Icon
                                 name='camera'
